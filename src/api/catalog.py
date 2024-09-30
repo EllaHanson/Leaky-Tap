400a83e89ddd386bbdd382fc6d537173905a3ae6
EllaHanson <ellahanson.126@gmail.com>
2024-09-30 11:00:28 -0700

    Get Catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,17 +15,16 @@ def get_catalog():
     """
 
     #version 1
-    """
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text(sql_to_execute))
-    """
+        green_potion_num = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
+    
 
     return [
             {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
+                "sku": "GREEN_POTION_0",
+                "name": "green potion",
+                "quantity": green_potion_num,
                 "price": 50,
-                "potion_type": [100, 0, 0, 0],
+                "potion_type": [0, 100, 0, 0],
             }
         ]
