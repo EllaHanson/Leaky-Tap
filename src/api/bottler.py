a6e9317bcaa047892f5ab2eb388b2938e516b4db
EllaHanson <ellahanson.126@gmail.com>
2024-10-06 14:53:47 -0700

    bottler plan version 2

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -44,22 +44,44 @@ def get_bottle_plan():
 
     #version 1
     with db.engine.begin() as connection:
-        result_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).fetchone()
-        green_ml = result_ml.num_green_ml
-        result_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).fetchone()
-        green_potion = result_green_potions.num_green_potions
+        #red potion info
+        result_red_potions = connection.execute(sqlalchemy.text("SELECT amount FROM potions WHERE color = 'red'")).fetchone()
+        red_potions = result_red_potions.amount
+        result_red_ml = connection.execute(sqlalchemy.text("SELECT amount FROM ml WHERE color = 'red'")).fetchone()
+        red_ml = result_red_ml.amount
+        #green potion info
+        result_green_potions = connection.execute(sqlalchemy.text("SELECT amount FROM potions WHERE color = 'green'")).fetchone()
+        green_potions = result_green_potions.amount
+        result_green_ml = connection.execute(sqlalchemy.text("SELECT amount FROM ml WHERE color = 'green'")).fetchone()
+        green_ml = result_green_ml.amount
+        #blue potion info
+        result_blue_potions = connection.execute(sqlalchemy.text("SELECT amount FROM potions WHERE color = 'blue'")).fetchone()
+        blue_potions = result_blue_potions.amount
+        result_blue_ml = connection.execute(sqlalchemy.text("SELECT amount FROM ml WHERE color = 'blue'")).fetchone()
+        blue_ml = result_blue_ml.amount
 
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
     # Initial logic: bottle all barrels into red potions.
+    return_list = []
 
-    if green_ml >= 100 and green_potion <= 10:
-        potion_num = green_ml / 100
-        return [{ "potion_type": [0, 100, 0, 0], "quantity": int(potion_num) }]
-    else:
-        return [] # return empty array for no bottling (??) i think
+    if red_ml >= 100 and red_potions <= 10:
+        red_potion_num = red_ml / 100
+        return_list.append({"potion_type": [100,0,0,0], "quantity": int(red_potion_num)})
+        print(return_list)
+
+    if green_ml >= 100 and green_potions <= 10:
+        green_potion_num = green_ml / 100
+        return_list.append({"potion_type": [0,100,0,0], "quantity": int(green_potion_num)})
+        
+    if blue_ml >= 100 and blue_potions <= 10:
+        blue_potion_num = blue_ml / 100
+        return_list.append({"potion_type": [0,0,100,0], "quantity": int(blue_potion_num)})
+
+    
+    return return_list # return empty array for no bottling (??) i think
 
 if __name__ == "__main__":
     print(get_bottle_plan())
