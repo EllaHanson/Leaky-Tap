04456609ae0367a26c74c68b32a4edd9f6436b45
EllaHanson <ellahanson.126@gmail.com>
2024-10-09 12:25:31 -0700

    audit

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 from src.api import auth
 import math
 
+import sqlalchemy
+from src import database as db
+
 router = APIRouter(
     prefix="/inventory",
     tags=["inventory"],
@@ -12,8 +15,27 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
+    print("performing audit")
+    with db.engine.begin() as connection:
+        potions_list = connection.execute(sqlalchemy.text("SELECT amount FROM potions")).fetchall()
+        ml_list = connection.execute(sqlalchemy.text("SELECT amount FROM ml")).fetchall()
+        res_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).fetchone()
+        gold = res_gold.gold
+
+        potion_count = 0
+        ml_count = 0
+        for n in potions_list:
+            potion_count += n[0]
+        for n in ml_list:
+            ml_count += n[0]
+
+        print("audit results:")
+        print("potions: ", potion_count)
+        print("ml: ", ml_count)
+        print("gold: ", gold)
+
     
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    return {"number_of_potions": potion_count, "ml_in_barrels": ml_count, "gold": gold}
 
 # Gets called once a day
 @router.post("/plan")
